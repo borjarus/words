aa2f2fd69febb15134782bfb68c0b9c1f66aaefa
Miroslaw Puczynski <miroslaw@puczynski.org>
2019-04-17 18:08:45 +0200

    move data code to Data.hs and write some Tests

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,6 +11,7 @@ module Lib
 
 import Data.List(isInfixOf, transpose)
 import Data.Maybe(catMaybes)
+import Data
 type Grid = [String]
 
 outputGrid :: Grid -> IO() 
@@ -52,29 +53,5 @@ findWords grid words =
 findWordInLine :: String -> String -> Bool
 findWordInLine =  isInfixOf
 
-grid = [ "__C________R___"
-       , "__SI________U__"
-       , "__HASKELL____B_"
-       , "__A__A_____S__Y"
-       , "__R___B___C____"
-       , "__PHP____H_____"
-       , "____S_LREP_____"
-       , "____I__M_Y__L__"
-       , "____L_E__T_O___"
-       , "_________HB____"
-       , "_________O_____"
-       , "________CN_____"
-       ]
 
-languages = [ "BASIC"
-            , "COBOL"
-            , "CSHARP"
-            , "HASKELL"
-            , "LISP"
-            , "PERL"
-            , "PHP"
-            , "PYTHON"
-            , "RUBY"
-            , "SCHEME"
-            ]
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,23 @@
 import Test.Hspec
 import Lib
+import Data
+
 
 main :: IO ()
 main = hspec $ do
-    describe "How to write a test?" $ do
-        it "Shold be able to run tests" $ do
-            someString `shouldBe` "someString" 
-            someString `shouldBe` "another String" 
+    describe "formatGrid" $ do
+        it "Should concatenate every line wit newline" $ do
+            (formatGrid ["abc", "def", "ghi"]) `shouldBe` "abc\ndef\nghi\n"
+
+    describe "findWord" $ do
+        it "Should find word that exist on the Grid" $ do
+            findWord grid "HASKELL" `shouldBe` Just "HASKELL"
+            findWord grid "PERL" `shouldBe` Just "PERL"
+        it "Should not find word that don't exist on the Grid" $ do
+            findWord grid "HAMSTER" `shouldBe` Nothing
+
+    describe "findWords" $ do 
+        it "Should find all the word that exist on the Grid" $ do 
+            findWords grid languages `shouldBe` languages
+        it "Should not find words that don't exist on the Grid" $ do 
+            findWords grid ["FRENCH", "GERMAN","ENGLISH"] `shouldBe` []
